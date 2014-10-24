d277c69b43b3c6f619a2bbe0f5fac2482fd727fd
Brice Shatzer <brice.shatzer@gmail.com>
2014-10-23 20:52:03 -0500

    Expanded role of src folder

---
+++ src/BestAvailable.js
@@ -0,0 +1,8 @@
+/*
+---BestAvailable.js---
+A simple web app with goal of helping you track and select available players during your live fantasy football draft.
+
+Built using angularJS & Bootstrap with data being provided by fantasyfootballnerd.com 
+
+See http://www.fantasyfootballnerd.com/fantasy-football-api for more info.
+*/
